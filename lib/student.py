25228bda98af6d48793d8d01328427531d19c3ec
Esther Wandera <wanderaesther809@gmail.com>
2025-12-08 01:13:19 +0300

    initial commit

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, info):
+        self.knowledge.append(info)
